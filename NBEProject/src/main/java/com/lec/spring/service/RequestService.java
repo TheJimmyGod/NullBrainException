4dfb55015dfe00fbac31892492d26af3888607a0
leegayeong <gayeong621@naver.com>
2024-06-27 16:21:56 +0900

    Purchase 도메인, RequestController, RequestRepository, RequestService, RequestServiceImpl, RequestRepository.xml 수정

---
+++ NBEProject/src/main/java/com/lec/spring/service/RequestService.java
@@ -0,0 +1,11 @@
+package com.lec.spring.service;
+
+import com.lec.spring.domain.Purchase;
+
+import java.util.List;
+
+public interface RequestService {
+
+    // 특정 유저의 물건들의 배송상태
+    List<Purchase> listPurchaseStatus(Integer userId);
+}
