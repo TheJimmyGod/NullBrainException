42ad6ee9e731b006f5fd9b5bc44d417457eceb91
TheJimmyGod <minjang115@gmail.com>
2024-07-03 17:10:45 +0900

    2024-7-3 진민장: 게시글 이미지 첨부에 미리보기를 추가, 양측의 배경화면 구현, 이미지들 새로 추가

--- NBEProject/src/main/resources/static/js/writeCode.js
+++ NBEProject/src/main/resources/static/js/writeCode.js
@@ -1,19 +1,120 @@
+let writeImageMap = new Map();
+let startIndex = 0;
 $(function (){
     // [추가] 버튼 누르면 첨부 파일 추가 가능
-    let i = 0;
     $("#btnAdd").click(function (){
-
-        if(i < 5)
+        if($("#files").children().length >= 5)
         {
-            $("#files").append(`<div class="input-group mb-2">
-   <input class="form-control col-xs-3" type="file" id="imageList" name="upfile${i}" accept="image/jpeg, image/png, image/jpg"/>
-   <button type="button" class="btn btn-outline-danger" onclick="$(this).parent().remove()">삭제</button>
-    </div>`);
-            i++;
+            alert("최대 5개까지 추가 가능합니다.");
         }
+
         else
         {
-            alert("5개까지 넣을 수 있습니다.");
+            if($("#files").children().length >= 1)
+            {
+                let fail = false;
+                $("[name^='upfile']").each(function (index, element){
+                    if($(this).get(0).files[0] === undefined)
+                    {
+                        alert("먼저 파일을 선택해주십시요");
+                        fail = true;
+                    }
+                });
+                if(fail)
+                {
+                    fail = false;
+                    return;
+                }
+            }
+            $("#files").append(`<div class="input-group mb-2">
+   <input class="form-control col-xs-3" type="file" id="imageList" name="upfile${startIndex}" accept="image/jpeg, image/png, image/jpg" onchange="Display(this, ${startIndex})"/>
+    </div>`);
+            startIndex++;
         }
     });
 });
+
+$(document).ready(function (){
+    $(document).on('click', "[class*='delBtn']",function ()
+    {
+        let _code = parseInt($(this).attr("id").match(/\d+/));
+        DeleteShortcut(_code);
+    });
+
+    $(document).on('mouseenter', "[class*='delBtn']",function ()
+    {
+        let _code = parseInt($(this).attr("id").match(/\d+/));
+        $("[name='upfile" + _code + "']").addClass("selectInput");
+    });
+    $(document).on('mouseleave', "[class*='delBtn']",function ()
+    {
+        let _code = parseInt($(this).attr("id").match(/\d+/));
+        $("[name='upfile" + _code + "']").removeClass("selectInput");
+    });
+});
+
+function DeleteShortcut(index)
+{
+    $("#shortcut" + index).parent().remove();
+    $("[name='upfile" + index + "']").parent().remove();
+    writeImageMap.set(index, {"active":false, "line":null})
+    $(`#shortcutBlock${index}`).remove();
+}
+
+function Display(input ,index)
+{
+    let file = input.files[0];
+    let $exist = $(`#shortcut${index}`);
+    if(input.files.length === 0)
+    {
+        if($exist != null)
+        {
+            DeleteShortcut(index);
+        }
+        return;
+    }
+
+    var img = URL.createObjectURL(file);
+    if(img === null)
+    {
+        alert("이미지에 오류가 발생했습니다.");
+        return;
+    }
+    else
+    {
+        if($exist != null)
+            $exist.parent().remove();
+        var $newShortcut = $('<img>');
+
+        $newShortcut.addClass('col-lg-2');
+        $newShortcut.addClass('col-sm-2');
+        $newShortcut.addClass('col-2');
+        $newShortcut.addClass(`delBtn${index}`);
+        $newShortcut.addClass("selectImage");
+        $newShortcut.attr('id', `shortcut${index}`);
+
+        $newShortcut.attr('src', img);
+
+        $newShortcut.css('width', '150px');
+        $newShortcut.css('height', '150px');
+        $newShortcut.css('visibility', 'visible');
+        $newShortcut.css('float','left');
+        $newShortcut.css('margin-right','2%');
+
+        let line = $(`<span id="shortcutBlock${index}"></span>`);
+        $(line).append($newShortcut);
+        var $container = $("#shortcutGroup");
+        $container.children().each(function (index,element){
+            $(this).remove();
+        });
+
+        writeImageMap.set(index, {"active":true, "line":line});
+        for (let z = 0; z < writeImageMap.size; z++) {
+            if(writeImageMap.get(z)["active"] === false)
+                continue;
+            else
+                $container.append(writeImageMap.get(z)["line"]);
+        }
+    }
+
+}
