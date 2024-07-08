6edb7135b0d00362bdf10aa9a0904002b2546d91
AA0027 <129959175+AA0027@users.noreply.github.com>
2024-07-08 09:36:41 +0900

    Update detail.js

--- NBEProject/src/main/resources/static/cho/js/prod/detail.js
+++ NBEProject/src/main/resources/static/cho/js/prod/detail.js
@@ -55,7 +55,7 @@ $(document).ready(function() {
     })
 
     cart.click(function (){
-        location.href = "../../cart";
+        location.href = "/cart";
     })
 
     $('.cart').click(function(){
